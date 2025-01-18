a42384e3dea8e6511ba383a3600032021907addd
Garvit Gandhi <gandhigarvit2712@gmail.com>
2025-01-18 12:55:25 +0000

    Code challenge submission for question: 2220 Minimum Bit Flips to Convert Number

---
+++ 2220 Minimum Bit Flips to Convert Number/2220minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int n= start^goal;
+        int count=0;
+
+        for(int i=0; i<31; i++){
+            if((n & (1<<i))!=0){
+                count++;
+            }
+        }
+        return count;
+
+
+    }
+};
