0a255736aee5586a9cd3cdab10971a55a25271e2
Garvit Gandhi <gandhigarvit2712@gmail.com>
2025-06-14 18:48:01 +0000

    Code challenge submission for question: 101 Symmetric Tree

---
+++ 101 Symmetric Tree/101symmetric-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool symtest(TreeNode* p, TreeNode* q){
+        if(p==NULL && q==NULL){
+            return true;
+        }
+        else if(p==NULL || q==NULL){
+            return false;
+        }
+         else if(p->val != q->val){
+            return false;}
+        return symtest(p->left,q->right) && symtest(p->right,q->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root==NULL){
+            return true;
+        }
+        return symtest(root->left,root->right);
+    }
+};
