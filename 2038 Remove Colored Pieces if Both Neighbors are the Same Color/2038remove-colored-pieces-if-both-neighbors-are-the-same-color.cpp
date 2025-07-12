713b03239793979b61f3cbaaa140e63383e35f9d
Garvit Gandhi <gandhigarvit2712@gmail.com>
2025-07-12 07:18:10 +0000

    Code challenge submission for question: 2038 Remove Colored Pieces if Both Neighbors are the Same Color

---
+++ 2038 Remove Colored Pieces if Both Neighbors are the Same Color/2038remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int n = colors.size();
+        if(n==1 || n==2){
+            return false;
+        }
+        int a = 0;
+        int b = 0;
+        for(int i=1; i<n-1; i++){
+            if(colors[i]=='A' && colors[i-1]=='A' && colors[i+1]=='A'){
+                a++;
+            }
+            else if(colors[i]=='B' && colors[i-1]=='B' && colors[i+1]=='B'){
+                b++;
+            }
+        }
+        while(a>0 && b>0){
+            a--;
+            b--;
+        }
+        if(a==0){
+            return false;
+        }
+        return true;
+    }
+};
