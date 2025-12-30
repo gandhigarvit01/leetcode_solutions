f17a2952d32d00d863b1df990e6571e7abe1998a
Garvit Gandhi <gandhigarvit2712@gmail.com>
2025-12-30 11:21:37 +0000

    Code challenge submission for question: 530 Minimum Absolute Difference in BST

---
+++ 530 Minimum Absolute Difference in BST/530minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> vec;
+    void inorder(TreeNode* root){
+        if(root==NULL){
+            return;
+        }
+        inorder(root->left);
+        vec.push_back(root->val);
+        inorder(root->right);
+    }
+    int getMinimumDifference(TreeNode* root) {
+        inorder(root);
+        int n = vec.size();
+        int mini = 1000000;
+        for(int i=0; i<n-1; i++){
+            mini = min(mini,abs(vec[i]-vec[i+1]));
+        }
+        return mini;
+    }
+};
