73d2883273c1fc3b56cedbf4f60317e7c3712140
Garvit Gandhi <gandhigarvit2712@gmail.com>
2026-02-11 05:21:49 +0000

    Code challenge submission for question: 100 Same Tree

---
+++ 100 Same Tree/100same-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool issame(TreeNode* p, TreeNode* q){
+        if(p==NULL && q==NULL) return true;
+        if(p==NULL || q==NULL) return false;
+        if(p->val!=q->val) return false;
+        bool l = issame(p->left,q->left);
+        bool r = issame(p->right,q->right);
+        return (l && r);
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return issame(p,q);
+    }
+};
