ddd5a9b713000df4d26659a5f789b84c3353793b
Garvit Gandhi <gandhigarvit2712@gmail.com>
2026-05-22 18:48:03 +0000

    Code challenge submission for question: 217 Contains Duplicate

---
+++ 217 Contains Duplicate/217contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> mpp;
+        for(int i=0; i<n; i++){
+            if(mpp.find(nums[i])!=mpp.end()) return true;
+            mpp[nums[i]]++;
+        }
+        return false;
+    }
+};
