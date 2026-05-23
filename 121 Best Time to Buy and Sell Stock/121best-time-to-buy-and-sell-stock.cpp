601d799c140f3db7145f7836aed21c3b4519eec3
Garvit Gandhi <gandhigarvit2712@gmail.com>
2026-05-23 12:08:09 +0000

    Code challenge submission for question: 121 Best Time to Buy and Sell Stock

---
+++ 121 Best Time to Buy and Sell Stock/121best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int ans = 0;
+        int mini = prices[0];
+        for(int i=0; i<n; i++){
+            ans = max(ans, prices[i]-mini);
+            mini = min(mini, prices[i]);
+        }
+        return ans;
+    }
+};
