975afdd61e0044ee1e9f1db8fc227568718dd25c
Garvit Gandhi <gandhigarvit2712@gmail.com>
2026-05-23 11:05:25 +0000

    Code challenge submission for question: 31 Next Permutation

---
+++ 31 Next Permutation/31next-permutation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int idx = -1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                idx = i;
+                break;
+            }
+        }
+        if(idx==-1){
+            sort(nums.begin(), nums.end());
+        }
+        else{
+            for(int i=n-1; i>idx; i--){
+                if(nums[i] > nums[idx]){
+                    int temp = nums[idx];
+                    nums[idx] = nums[i];
+                    nums[i] = temp;
+                    break;
+                }
+            }
+            sort(nums.begin()+idx+1, nums.end());
+        }
+    }
+};
