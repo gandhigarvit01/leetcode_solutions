18211601b13a1f1679800d30f406ae9c535cdf70
Garvit Gandhi <gandhigarvit2712@gmail.com>
2026-06-22 07:17:15 +0000

    Code challenge submission for question: 347 Top K Frequent Elements

---
+++ 347 Top K Frequent Elements/347top-k-frequent-elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> mpp;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            mpp[nums[i]]++;
+        }
+        priority_queue<pair<int,int>> pq;
+        vector<int> ans;
+        for(auto it : mpp){
+            pq.push({it.second,it.first});
+        }
+        while(k--){
+            auto p = pq.top();
+            pq.pop();
+            ans.push_back(p.second);
+        }
+        return ans;
+    }
+};
