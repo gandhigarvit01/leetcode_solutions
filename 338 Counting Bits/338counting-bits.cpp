4bee50a47439feaff102daf291abcfbe432ca5c2
Garvit Gandhi <gandhigarvit2712@gmail.com>
2026-06-29 06:46:42 +0000

    Code challenge submission for question: 338 Counting Bits

---
+++ 338 Counting Bits/338counting-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1,0);
+        ans[0] = 0;
+        for(int i=1; i<=n; i++){
+            ans[i] = ans[i/2];
+            if(i&1) ans[i]++;
+        }
+        return ans;
+    }
+};
