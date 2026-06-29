3a0fe8f4dcc76e45c73ea16e24d11ee8e7dc9a10
Garvit Gandhi <gandhigarvit2712@gmail.com>
2026-06-29 06:35:10 +0000

    Code challenge submission for question: 137 Single Number II

---
+++ 137 Single Number II/137single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int num = 0;
+        int n = nums.size();
+        for(int i=0; i<32; i++){
+            int cnt = 0;
+            for(int j=0; j<n; j++){
+                if(((nums[j]>>i)&1) == 1) cnt++;
+            }
+            if(cnt%3 != 0){
+                num |= (1<<i);
+            }
+        }
+        return num;
+    }
+};
